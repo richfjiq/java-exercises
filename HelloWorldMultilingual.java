dc9d601ca37ebf8d60aa7a60b75c0e95d63671b1
richfjiq <rfjiq1986@gmail.com>
2023-04-15 20:39:54 -0600

    basic programming concepts - java exercises

---
+++ HelloWorldMultilingual.java
@@ -0,0 +1,14 @@
+public class HelloWorldMultilingual {
+  public static void main(String[] args) {
+    System.out.println("Witaj świecie");
+    System.out.println("Olá Mundo");
+    System.out.println("Salut Lume");
+    System.out.println("Halo Dunia");
+    System.out.println("Talofa, Lalolagi");
+    System.out.println("Здраво Свете");
+    System.out.println("Ciao mondo");
+    System.out.println("こんにちは世界");
+    System.out.println("Hola món");
+    System.out.println("Hallo Wereld");
+  }
+}
